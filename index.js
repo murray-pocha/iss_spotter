aa1b2a2032cb196776fa9c0d8f5e5dd95c05a7a2
murray-pocha <murray.pocha@gmail.com>
2024-11-13 13:50:33 -0700

    adjusted code to require and use the new function fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const { fetchMyIp } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-fetchMyIp((error, ip) => {
+fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
 
   console.log('It worked! Returned IP:', ip);
+
+
+  fetchCoordsByIP(ip, (error, coords) => {
+    if (error) {
+      console.log("It didnt work, error");
+      return;
+    }
+
+    console.log('Coordinates for IP:', coords);
+    console.log(`Latitude: ${coords.latitude}, Longitude: ${coords.longitude}`);
+  });
 });
