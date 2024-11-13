55c0454d2d0fe27ad5b1b7859e84616928e66a53
murray-pocha <murray.pocha@gmail.com>
2024-11-13 13:49:49 -0700

    added fetchCoordsByIP function

--- iss.js
+++ iss.js
@@ -1,7 +1,7 @@
 const needle = require('needle');
 
 
-const fetchMyIp = function(callback) {
+const fetchMyIP = function(callback) {
   const url = 'https://api.ipify.org?format=json';
 
   needle.get(url, (err, response) => {
@@ -20,4 +20,29 @@ const fetchMyIp = function(callback) {
   });
 };
 
-module.exports = { fetchMyIp };
+
+const fetchCoordsByIP = function(ip, callback) {
+  const url = `https://ipwho.is/${ip}`;
+
+  needle.get(url, (err, response) => {
+    if (err) {
+      callback(`Error fetching coordinates: ${err.message}`);
+      return;
+    }
+
+    if (response.statusCode === 200) {
+
+      const { latitude, longitude } = response.body;
+
+      if (response.body.success) {
+        callback(null, { latitude, longitude });
+      } else {
+        callback(` Failed to fetch coordinates ${ip}. Error: ${response.body.message}`);
+      }
+    } else {
+      callback(`Failed to fetch coordinates. Status code: ${response.statusCode}`);
+    }
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
