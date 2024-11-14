b8636063482cace1ac188ef8f0493524f42d2606
murray-pocha <murray.pocha@gmail.com>
2024-11-13 19:05:28 -0700

    removed redundant code and cleaned everything up, checked for grammer

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
+const { nextISSTimesForMyLocation } = require('./iss');
 
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
 nextISSTimesForMyLocation((error, flyoverTimes) => {
   if (error) {
-    console.log("It didnt work.", error);
+    console.log("It didn't work.", error);
     return;
   }
 
@@ -12,45 +12,5 @@ nextISSTimesForMyLocation((error, flyoverTimes) => {
   flyoverTimes.forEach((time, index) => {
     const date = new Date(time.risetime * 1000);
     console.log(`${index + 1}. ${date.toUTCString()} (Duration: ${time.duration} seconds)`);
-  });
-});
-
-
-
-
-
-// fetch IP first
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
-
-  console.log('It worked! Returned IP:', ip);
-
-  // fetch coordinates for the IP address
-  fetchCoordsByIP(ip, (error, coords) => {
-    if (error) {
-      console.log("It didnt work.", error);
-      return;
-    }
-
-    console.log('Coordinates for IP:', coords);
-    console.log(`Latitude: ${coords.latitude}, Longitude: ${coords.longitude}`);
-
-
-    fetchISSFlyOverTimes(coords, (error, flyoverTimes) => {
-      if (error) {
-        console.log("It didn't work", error);
-        return;
-      }
-      // printing the flyover times
-      console.log('ISS Flyover Times:');
-      flyoverTimes.forEach((time, index) => {
-        const date = new Date(time.risetime * 1000); //converting timestamp to date
-        console.log(`${index + 1}. ${date.toUTCString()} (Duration: ${time.duration} seconds)`);
-      });
-
-    });
   });
 });
